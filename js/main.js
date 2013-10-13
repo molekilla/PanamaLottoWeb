d4511987025919164f3e7145e0651207f1ab966e
molekilla <rogelio@rightscale.com>
2013-10-13 18:06:19 -0500

    bootstrap, odometer, and init style

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
 require.config({
     paths: {
-        "jquery": ["vendor/jquery/jquery"],
-        "underscore": "vendor/underscore/underscore-min",
-        "backbone": "vendor/backbone/backbone-min",
-        "handlebars": "vendor/handlebars/handlebars"
+      "jquery": ["vendor/jquery/jquery"],
+      "underscore": "vendor/underscore/underscore-min",
+      "backbone": "vendor/backbone/backbone-min",
+      "handlebars": "vendor/handlebars/handlebars",
+      "bootstrap": "vendor/bootstrap/dist/js/bootstrap.min"
     },
     shim: {
-        'underscore': {
-           'exports': '_'
-         },
-         'backbone': {
-           'deps': ['jquery', 'underscore'],
-           'exports': 'Backbone'
-         },
-         'handlebars': {
-           'exports': 'Handlebars'
-         }
+      'underscore': {
+         'exports': '_'
+       },
+       'backbone': {
+         'deps': ['jquery', 'underscore'],
+         'exports': 'Backbone'
+       },
+       'handlebars': {
+         'exports': 'Handlebars'
+       },
+       'bootstrap' : {
+        'deps' : ['jquery'],
+        'exports' : 'bs'
+       }
     },
     waitSeconds: 8
 });
 //requiring the scripts in the first argument and then passing the library namespaces into a callback
 //you should be able to console log all of the callback arguments
-require(['jquery', 'underscore', 'backbone', 'app'], function(jquery, _, Backbone, App){
+require(['jquery', 'underscore', 'backbone', 'app', 'bootstrap'], function(jquery, _, Backbone, App, bs){
   var app = new App();
   app.init();
 });
